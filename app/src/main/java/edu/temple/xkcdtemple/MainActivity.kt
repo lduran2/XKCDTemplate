efd8fecea1f8ce98d1e5bbb30b8cde7de2038bf3
Leomar Duran <tuh24865@temple.edu>
2022-10-27 11:35:35 -0400

    fetching data as a string

--- app/src/main/java/edu/temple/xkcdtemple/MainActivity.kt
+++ app/src/main/java/edu/temple/xkcdtemple/MainActivity.kt
@@ -14,15 +14,30 @@ class MainActivity : AppCompatActivity() {
     lateinit var comicNumberEditText: EditText
     lateinit var loadComicButton: Button
     lateinit var comicDisplayImageView: ImageView
+	
+	lateinit var requestQueue : RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+		
+		requestQueue = Volley.newRequestQueue(this)
 
         titleTextView = findViewById(R.id.titleTextView)
         altTextTextView = findViewById(R.id.altTextTextView)
         comicNumberEditText = findViewById(R.id.comicNumberEditText)
         loadComicButton = findViewById(R.id.loadComicButton)
         comicDisplayImageView = findViewById(R.id.comicDisplayImageView)
+		
+		loadComicButton.setOnClickListener {
+			val comicNumber = comicNumberEditText.text.toString()
+			val uri = URI("https://xkcd.com/$comicNumber/info.0.json")
+
+            requestQueue.add(
+                JsonObjectRequest(Request.Method.GET, uri.toString(), null, {
+                    Log.d("Response", it.toString())
+                }, {})
+            )
+		}
     }
 }
